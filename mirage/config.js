86a086ba35ac64489105ae179525bfe74d7dce0d
Sarup Banskota <sbanskota08@gmail.com>
2016-11-27 21:13:09 +0200

    Setup Mirage for teams

--- mirage/config.js
+++ mirage/config.js
@@ -9,18 +9,10 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  // this.namespace = 'api';
+  this.timing = 400;
 
-  /*
-    Shorthand cheatsheet:
+  this.get('/teams');
+  this.get('/teams/:id');
 
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
-  */
 }
