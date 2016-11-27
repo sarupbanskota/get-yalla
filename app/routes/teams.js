5f39665c5c41bf32e8f2d73ee367182e4d93bd26
Sarup Banskota <sbanskota08@gmail.com>
2016-11-27 15:49:26 +0200

    Create teams route

---
+++ app/routes/teams.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return [{
+      name: 'Dotnet',
+      count: 12,
+      description: 'parsing and normalizing'
+    }, {
+      name: 'Ruby',
+      count: 4,
+      description: 'backend'
+    }, {
+      name: 'Frontend',
+      count: 4,
+      description: 'building interfaces'
+    }];
+  }
+});
