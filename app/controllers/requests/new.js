d7f47784f6c9b4e13927260f9f357ad9336be55a
Sarup Banskota <sbanskota08@gmail.com>
2016-12-03 17:28:39 +0200

    Validate that new request content is valid

--- app/controllers/requests/new.js
+++ app/controllers/requests/new.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
-const { inject: { service } } = Ember;
+const { inject: { service }, computed } = Ember;
 
 export default Ember.Controller.extend({
   session: service(),
   from: new Date(),
   to: new Date(),
   description: undefined,
-  extraPickadateOptions: {
-    clear: '',
-    format: "d mmm"
-  },
+
+  fromValid : computed.empty('from'),
+  toValid: computed.empty('to'),
+  descriptionValid: computed.empty('description'),
+  incompleteRequest: computed.or('fromValid', 'toValid', 'descriptionValid'),
+
   actions: {
     createNewRequest() {
       this.get('store').createRecord('request', {
@@ -19,6 +21,14 @@ export default Ember.Controller.extend({
         description: this.get('description'),
         username: this.get('session.session.content.authenticated.profile.name')
       }).save();
+      this.set('from', new Date());
+      this.set('to', new Date());
+      this.set('description', undefined);
     }
+  },
+
+  extraPickadateOptions: {
+    clear: '',
+    format: "d mmm"
   }
 });
