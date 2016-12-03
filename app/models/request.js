de56c7ff5f9602abfda8abaf69e0a8c147ac31f8
Sarup Banskota <sbanskota08@gmail.com>
2016-12-03 22:37:58 +0200

    Log actual duration of request

--- app/models/request.js
+++ app/models/request.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import moment from 'moment';
 import computed from 'ember-computed';
 
 export default DS.Model.extend({
@@ -16,5 +17,11 @@ export default DS.Model.extend({
       case 'Accepted': return 'success';
       case 'Rejected': return 'danger';
     };
+  }),
+  duration: computed('from', 'to', function() {
+    const fromMoment = moment(this.get('from'));
+    const toMoment = moment(this.get('to'));
+    const difference = Math.abs(fromMoment.diff(toMoment, 'days'));
+    return difference === 0 ?  "1 day" :`${difference + 1} days`;
   })
 });
