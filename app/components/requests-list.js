94e011be703d534021e5ca3eafc21354e403a29c
Sarup Banskota <sbanskota08@gmail.com>
2016-12-03 02:28:31 +0200

    Allow updating status

--- app/components/requests-list.js
+++ app/components/requests-list.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
+const { inject: { service } } = Ember;
 
 export default Ember.Component.extend({
+  store: service(),
+
+  actions: {
+    updateRequestStatus: function(request) {
+      let updatedRequest = this.get('store').peekRecord('request', request.id);
+      updatedRequest.set('status', 'Accepted');
+      updatedRequest.save();
+    }
+  }
 });
