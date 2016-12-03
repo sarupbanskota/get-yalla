61ac44377f8f65b61800cde212b212a512ac1e27
Sarup Banskota <sbanskota08@gmail.com>
2016-12-03 13:18:48 +0200

    Update color based on status of leave

--- app/models/request.js
+++ app/models/request.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import computed from 'ember-computed';
 
 export default DS.Model.extend({
   requestedBy: DS.attr('string'),
@@ -7,5 +8,12 @@ export default DS.Model.extend({
   to: DS.attr('string'),
   description: DS.attr('string'),
   status: DS.attr('string'),
-  type: DS.attr('string')
+  type: DS.attr('string'),
+  statusClass: computed('status', function() {
+    switch(this.get('status')) {
+      case 'Pending': return 'secondary';
+      case 'Accepted': return 'success';
+      case 'Rejected': return 'danger';
+    };
+  })
 });
