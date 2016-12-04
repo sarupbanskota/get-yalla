5d5d10135055b561a33fdf0ea0123a873cb082d5
Sarup Banskota <sbanskota08@gmail.com>
2016-12-04 22:36:58 +0200

    Allow filtering based on status

--- app/controllers/requests.js
+++ app/controllers/requests.js
@@ -2,12 +2,27 @@ import Ember from 'ember';
 const { computed, inject: { service } } = Ember;
 
 export default Ember.Controller.extend({
+  queryParams: ['status', 'username'],
+  status: null,
   session: service(),
+  statusClass: computed('status', function() {
+    switch(this.get('status')) {
+      case 'Pending': return 'primary';
+      case 'Accepted': return 'success';
+      case 'Rejected': return 'danger';
+      default: return 'default';
+    };
+  }),
   isOwner: computed('session', function() {
     return this.get('session.data.authenticated.profile.role') === 'owner';
   }),
-  requestsListTitle: computed('isOwner', function() {
-    if (this.get('isOwner')) return 'Listing requests across the entire organisation';
-    else return 'Listing requests from you';
+  requestsListTitle: computed('isOwner', 'status', function() {
+    const status = !this.get('status') ? 'All' : this.get('status');
+    const statusTag = `<span class='tag tag-${this.get('statusClass')}'> ${status} </span>`;
+    if (this.get('isOwner')) {
+      return `You asked for ${statusTag} requests across the entire organisation`;
+    } else {
+      return `You asked for ${statusTag} requests from you`;
+    }
   })
 });
--- app/routes/requests.js
+++ app/routes/requests.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model() {
-    return this.get('store').findAll('request');
+  queryParams: {
+    status: {
+      refreshModel: true
+    }
+  },
+  model(params) {
+    return this.get('store').query('request', params);
   }
 });
