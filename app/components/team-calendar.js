d1c0315a60097dc6297c14f4fd30fd2597803460
Sarup Banskota <sbanskota08@gmail.com>
2016-12-10 04:39:48 +0200

    Plug in users data in calendar

--- app/components/team-calendar.js
+++ app/components/team-calendar.js
@@ -4,6 +4,8 @@ import moment from 'moment';
 export default Ember.Component.extend({
   date: new Date(),
   calendar: null,
+  viewingPeriod: null,
+  users: ['Sarup', 'Martin', 'Sarit'],
   data: [{
     username: 'Sarup',
     requests: [{
@@ -14,23 +16,70 @@ export default Ember.Component.extend({
   }, {
     username: 'Martin',
     requests: [{
-      from: '13-12-2016',
-      to: '15-12-2016',
+      from: '7-12-2016',
+      to: '8-12-2016',
       description: 'Getting hitched'
     }, {
-      from: '18-04-2017',
-      to: '20-04-2017',
+      from: '9-12-2016',
+      to: '12-12-2016',
       description: 'Conference'
     }]
   }, {
     username: 'Sarit',
     requests: [{
-      from: '25-12-2016',
-      to: '28-12-2016',
+      from: '6-12-2016',
+      to: '9-12-2016',
       description: 'Take Shakira for a walk'
     }]
   }],
+  init() {
+    this._super();
+    let startOfPeriod = moment().startOf('week');
+    let endOfPeriod = moment().endOf('week');
+    let day = startOfPeriod;
+    let daysInPeriod = [];
+
+    while (day <= endOfPeriod) {
+      daysInPeriod.push(day.format('DD-MM-YYYY'));
+      day = day.clone().add(1, 'd');
+    }
+
+    this.set('viewingPeriod', daysInPeriod);
+  },
   actions: {
+    makeCalendar() {
+      let startOfPeriod = moment().startOf('week');
+      let endOfPeriod = moment().endOf('week');
+      this.get('data').forEach(user => {
+        user.requests.forEach(request => {
+          let fromMoment = moment(request.from, 'DD-MM-YYYY');
+          let toMoment = moment(request.to, 'DD-MM-YYYY');
+
+          if (fromMoment.isBetween(startOfPeriod, endOfPeriod)) {
+            let soonerDate = toMoment.isSameOrBefore(endOfPeriod) ? toMoment : endOfPeriod;
+            let duration = Math.abs(fromMoment.diff(soonerDate, 'days')) + 1;
+
+            console.log(`${request.description} : ${duration}`);
+
+            let userFromCell = Ember.$(`#${user.username}-${fromMoment.format('DD-MM-YYYY')}`);
+            userFromCell.attr('colspan', duration);
+            userFromCell.addClass('onVacation');
+            userFromCell.html(request.description);
+
+            let day = fromMoment.add(1, 'd');
+            while (day <= soonerDate) {
+              let userDayCell = Ember.$(`#${user.username}-${day.format('DD-MM-YYYY')}`);
+              userDayCell.remove();
+              userDayCell.addClass('onVacation');
+              day = day.clone().add(1, 'd');
+            }
+
+          } else {
+            console.log('request is outside, no bother');
+          }
+        });
+      });
+    },
     displayThings() {
 
       // while (day <= endOfWeek) {
@@ -65,17 +114,7 @@ export default Ember.Component.extend({
         user.requests.forEach((request) => {
           fromMoment = moment(request.from, 'DD-MM-YYYY');
           toMoment = moment(request.to, 'DD-MM-YYYY');
-          if (fromMoment.isBetween(startOfPeriod, endOfPeriod)) {
-            soonerDate = toMoment.isSameOrBefore(endOfPeriod) ? toMoment : endOfPeriod;
-            day = fromMoment;
-            while (day <= soonerDate) {
-              includedDays[day.format('DD-MM-YYYY')][user.username] = request.description;
-              day = day.clone().add(1, 'd');
-            }
 
-          } else {
-            console.log('request is outside, no bother');
-          }
         });
       });
 
