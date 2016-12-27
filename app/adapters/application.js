875013df928448f5bafc80453da6896caddc3ac4
Sarup Banskota <sbanskota08@gmail.com>
2016-12-27 21:37:36 +0200

    Introduce surge

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,5 +4,6 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 const { JSONAPIAdapter } = DS;
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:auth0-authorizer'
+  authorizer : 'authorizer:auth0-authorizer',
+  host       : 'http://178.62.88.113'
 });
