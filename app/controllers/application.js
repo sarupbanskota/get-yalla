9fa0f2cce77ab9cf074bc3c181051cffd41ed86b
Sarup Banskota <sbanskota08@gmail.com>
2016-12-29 22:31:46 +0200

    Improve experience for admin users

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
+import computed from 'ember-computed';
 
 const { Controller, inject } = Ember;
 
 export default Controller.extend({
-  session: inject.service()
+  session: inject.service(),
+
+  userRole: computed('session.data.authenticated.profile.role', function() {
+    return this.get('session.data.authenticated.profile.role') === 'owner' ? 'owner' : 'user';
+  })
 });
