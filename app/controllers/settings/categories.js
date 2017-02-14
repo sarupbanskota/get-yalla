21ca3311d25ddb1f06445b05ef60fc01a1d2d150
Sarup Banskota <sbanskota08@gmail.com>
2017-02-15 00:15:03 +0800

    Allow editing categories

--- app/controllers/settings/categories.js
+++ app/controllers/settings/categories.js
@@ -3,29 +3,52 @@ import Ember from 'ember';
 const { Controller, computed } = Ember;
 
 export default Controller.extend({
-  showingNewCategoryForm: false,
-  newCategoryName: null,
-  newCategoryColor: null,
-  buttonMessage: computed('showingNewCategoryForm', function() {
+  showingCategoryForm : false,
+  categoryName        : null,
+  categoryColor       : null,
+  buttonMessage       : computed('showingNewCategoryForm', function() {
     return this.get('showingNewCategoryForm') ? 'Close' : 'Add new category';
   }),
 
   actions: {
-    toggleNewCategoryForm() {
-      this.toggleProperty('showingNewCategoryForm');
+    toggleCategoryForm(id) {
+      if (id) {
+        let category = this.store.peekRecord('category', id);
+        this.set('showingCategoryForm', true);
+        this.set('editingCategoryId', id);
+        this.set('categoryName', category.get('name'));
+        this.set('categoryColor', category.get('color'));
+      } else {
+        this.toggleProperty('showingCategoryForm');
+      }
     },
-    createNewCategory() {
-      this.store.createRecord('category', {
-        name  : this.get('newCategoryName'),
-        color : this.get('newCategoryColor')
-      }).save().then(() => {
-        this.set('newCategoryName', null);
-        this.set('newCategoryColor', null);
-        this.send('toggleNewCategoryForm');
-      }).catch((error) => {
-        console.log("Couldn't create new category");
-        console.log(error);
-      });
+    createOrEditCategory(id) {
+      if (id) {
+        this.store.findRecord('category', id).then((category) => {
+          category.set('name', this.get('categoryName'));
+          category.set('color', this.get('categoryColor'));
+          category.save().then(() => {
+            this.set('categoryName', null);
+            this.set('categoryColor', null);
+            this.send('toggleCategoryForm');
+          }).catch((error) => {
+            console.log("Couldn't edit category");
+            console.log(error);
+          });
+        });
+      } else {
+        this.store.createRecord('category', {
+          name  : this.get('categoryName'),
+          color : this.get('categoryColor')
+        }).save().then(() => {
+          this.set('categoryName', null);
+          this.set('categoryColor', null);
+          this.send('toggleCategoryForm');
+        }).catch((error) => {
+          console.log("Couldn't create new category");
+          console.log(error);
+        });
+      }
     }
   }
 });
